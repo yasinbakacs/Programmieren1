12891f2a7fbd14261bef29f679ac1f27ec97cf50
Yasin <bakacs.yasin-it25@it.dhbw-ravensburg.de>
2026-03-04 13:08:38 +0100

    Hinzufügen funktionen

--- courses/coding-in-C/Lab_7/mysolutions/Section3.c
+++ courses/coding-in-C/Lab_7/mysolutions/Section3.c
@@ -33,20 +33,20 @@ Song *add_song(Playlist *playlist, const char *title, const char *artist){
     }
     
     newSong->artist = strdup(artist);
-    if (newSong->artist[49] == NULL){
+    if (newSong->artist == NULL){
         printf("Fehler bei Datenspeicher Zuweisung\n");
         free(newSong);
         return NULL;
     }
     
     newSong->title = strdup(title);
-    if (newSong->title[49] == NULL){
+    if (newSong->title == NULL){
         printf("Fehler bei Datenspeicher Zuweisung\n");
-        free(newSong->artist[49]);
+        free(newSong->artist);
         free(newSong);
         return NULL;
     }
-    newSong->title[49] = &title;
+    
     newSong->next = NULL;
     Song *current = playlist->head;
     while (current != NULL && current->next != NULL) {
@@ -57,16 +57,49 @@ Song *add_song(Playlist *playlist, const char *title, const char *artist){
     } else {
         current->next = newSong;
     }
+    
     return newSong;
-
+    
     free(newSong);
 }
 
+//@brief Prints out the Playlist with Song title and artist
+//
+//@param [in] Playlist *playlist a pointer to the playlist that needs printing.
+
+void print_List(const Playlist *playlist){
+    Song *temp = playlist->head;
+    while (temp != 0){
+        printf("Artist: %s\n", temp->artist);
+        printf("Title: %s\n", temp->title);
+        temp = temp->next;
+    }
+}
+
+
+//@brief Deletes the first song of the playlist.
+//
+//@param [in] Playlist *playlist a pointer to the playlist where the first song is to be deleted.
+
+void delete_firstSong(Playlist *playlist){
+    Song *temp = playlist->head;
+
+    playlist->head = temp->next;
+
+    free(temp->artist);
+    free(temp->title);
+    free(temp);
+}
 
 
 void main(){
     Playlist myPlaylist;
     init_playlist(&myPlaylist);
-    add_song(&myPlaylist, "Bohemian Rhapsody", "Queen");
-    add_song(&myPlaylist, "Stairway to Heaven", "Led Zeppelin");
+    add_song(&myPlaylist, "Crawling", "Linkin Park");
+    add_song(&myPlaylist, "Layla", "Eric Claptonn");
+    add_song(&myPlaylist, "Esperanto", "Max Herre");
+    print_List(&myPlaylist);
+    delet_firstSong(&myPlaylist);
+    print_List(&myPlaylist);
+    delete_Playlist(&myPlaylist);
 }
