e147af28a4d94c1fc5a8e2462f7846522cba0169
Yasin <bakacs.yasin-it25@it.dhbw-ravensburg.de>
2026-04-20 14:52:51 +0200

    compiler sachen

--- courses/coding-in-C++/Lab_1/Test.cpp
+++ courses/coding-in-C++/Lab_1/Test.cpp
@@ -20,13 +20,36 @@ int main(){
     std::uint8_t AGE_MAX = 130;
     int age = 0;
 
-    std::cout << "Enter your age: ";
-
-    if(!(std::cin >> age)){
-        std::cout << "Not a valid input.";
+    std::cout << "Enter age: ";
+    while(true){
+        if(!(std::cin >> age)){
+            std::cout << "Not a valid input. Try again. \n";
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     }
 
-    else if(age < 0){
-        std::cout << "No negative ages allowed.";
+        else if(age < 0){
+            std::cout << "No negative ages allowed. Try again. \n";
     }
+        else if(age > AGE_MAX){
+            std::cout << "Unrealistic age. Try again. \n";
+    }   
+        else {
+            break;
+    }
+        std::cout << "Enter age: ";
+    }
+    
+    std::cout << "You are " << age << " years old. \n";
+    if (validation::Senior(age)){
+        std::cout << "You are a Senior. \n";
+    }
+    else if (validation::Adult(age)){
+        std::cout << "You are an Adult. \n";
+    }
+    else {
+        std::cout << "You are a child. \n";
+    }
+    
+
 }
