cc314dcd29edcf565c30dfc0f9cea1acc2f88b67
Yasin <bakacs.yasin-it25@it.dhbw-ravensburg.de>
2026-04-23 11:09:36 +0200

    Add LAb2 Section 1 finished

--- courses/coding-in-C++/Lab_2/mySolution/Section1.cpp
+++ courses/coding-in-C++/Lab_2/mySolution/Section1.cpp
@@ -10,21 +10,60 @@ class BankAccount{
     void setOwner(std::string ownerName){
         owner = ownerName;
     }
-    std::getOwner(){
+    std::string getOwner(){
         return owner;
     }
     void deposit(double amount){
         if (amount <= 0){
-            std::cout <<"Invalid deposit amount." << std::endl;
+            std::cout <<"Invalid deposit amount. \n" << std::endl;
             return;
         }
         balance += amount;
     }
+
     void withdraw(double amount){
-        if (amount  > balance){
-            std::cout << "Not enough money on account." << std::endl;
+        if (amount < 0){
+            std::cout << "Invalid withdraw amount. \n";
         }
-        balance -= amount;
-        
+        if (amount  > balance){
+            std::cout << "Not enough money on account. \n" << std::endl;
+        }else{
+            balance -= amount;
+        }
     }
+
+    double getBalance();
+    void getAccountInfo();
+
+};
+
+
+double BankAccount::getBalance(){
+    return balance;
+}
+void BankAccount::getAccountInfo(){
+    std::cout << "Owner: " << getOwner() << std::endl;
+    std::cout << "Balance: " << getBalance() << std::endl;
+}
+
+int main(){
+
+    BankAccount myAcc;
+    myAcc.setOwner("Bobby");
+
+    myAcc.getAccountInfo();
+
+    myAcc.deposit(15.15);
+
+    myAcc.getAccountInfo();
+
+    myAcc.withdraw(15.16);
+
+    myAcc.deposit(-12.3);
+
+    myAcc.withdraw(15.14);
+
+    myAcc.getAccountInfo();
+
+
 }
