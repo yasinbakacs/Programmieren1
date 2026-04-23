5ee85748759959c5ef2a5acb94b1f4733c115828
Yasin <bakacs.yasin-it25@it.dhbw-ravensburg.de>
2026-04-23 12:03:43 +0200

    Lab2 Section2 fertig

--- courses/coding-in-C++/Lab_2/mySolution/Section2.cpp
+++ courses/coding-in-C++/Lab_2/mySolution/Section2.cpp
@@ -9,10 +9,36 @@ class Note{
 
     public:
 
-    Note(std::string text_input){
+    Note(std::string input){
         text = new std::string;
-        *text = text_input;
+        *text = input;
     }
 
+    Note(const Note &otherNote){
+        text = new std::string;
+        *text = *otherNote.text;
+    }
 
+    ~Note(){
+        delete text;
+        text = nullptr;
+        std::cout << "Memory released. \n";
+    }
+
+    void display();
+
+};
+
+
+void Note::display(){
+    std::cout << *text << std::endl;
+}
+
+int main(){
+
+    Note text1("Hallo was geht");
+    text1.display();
+
+    Note text2(text1);
+    text2.display();
 }
